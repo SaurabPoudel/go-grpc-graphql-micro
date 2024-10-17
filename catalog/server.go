b1b0ea9f218e10633de7a55e3c0cca10cabf77eb
SaurabPoudel <poudelsaurab20@gmail.com>
2024-10-17 12:38:32 +0545

    catalog protocol

---
+++ catalog/server.go
@@ -0,0 +1,35 @@
+package catalog
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	pb.UnimplementedAccountServiceServer
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv, &grpcServer{
+		UnimplementedAccountServiceServer: pb.UnimplementedAccountServiceServer{},
+		service:                           s,
+	})
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostProduct(ctx context.Context, r *pb.PostProductRequest) {}
+
+func (s *grpcServer) GetProduct(ctx context.Context, r *pb.GetProductRequest) {}
+
+func (s *grpcServer) GetProducts(ctx context.Context, r *pb.GetProductsRequest) {}
