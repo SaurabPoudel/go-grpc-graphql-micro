163c87d6e09ab88dc443d9a043d9a8fcadbf11e9
SaurabPoudel <poudelsaurab20@gmail.com>
2024-10-18 12:25:13 +0545

    junkhead

---
+++ order/repository.go
@@ -0,0 +1,76 @@
+package order
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/lib/pq"
+)
+
+type Repository interface {
+	Close()
+	PutOrder(ctx context.Context, o Order) error
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]order, error)
+}
+
+type postgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(url string) (Repository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &postgresRepository{db}, nil
+}
+
+func (r *postgresRepository) Close() {
+	r.db.Close()
+}
+
+func (r *postgresRepository) PutOrder(ctx context.Context, o Order) (err error) {
+	r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+	tx.ExecContext(
+		ctx,
+		"INSERT INTO orders(id, created_at, account_id, total_price) VALUES ($1, $2, $3, $4)",
+		o.ID,
+		o.CreatedAt,
+		o.AccountID,
+		o.TotalPrice,
+	)
+	if err != nil {
+		return
+	}
+
+	stmt, _ := tx.PrepareContext(ctx, pq.CopyIn("order_products", "order_id", "product_id", "quantity"))
+	for _, p := range o.Products {
+		_, err = stmt.ExecContext(ctx, o.ID, p.ID, p.quantity)
+		if err != nil {
+			return
+		}
+	}
+
+	_, err = stmt.ExecContext(ctx)
+	if err != nil {
+		return
+	}
+	stmt.Close()
+	return
+
+}
